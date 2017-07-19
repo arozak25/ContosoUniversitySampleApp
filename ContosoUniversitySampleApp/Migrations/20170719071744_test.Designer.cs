98e465140e70fcc2acb2b6efbe15b8583c2c5935
arozak25 <arozak25@gmail.com>
2017-07-19 14:19:40 +0700

    Migrations

---
+++ ContosoUniversitySampleApp/Migrations/20170719071744_test.Designer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ContosoUniversitySampleApp.Data;
+
+namespace ContosoUniversitySampleApp.Migrations
+{
+    [DbContext(typeof(SchoolContext))]
+    [Migration("20170719071744_test")]
+    partial class test
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ContosoUniversitySampleApp.Models.Course", b =>
+                {
+                    b.Property<int>("CourseID");
+
+                    b.Property<int>("Credits");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("CourseID");
+
+                    b.ToTable("Course");
+                });
+
+            modelBuilder.Entity("ContosoUniversitySampleApp.Models.Enrollment", b =>
+                {
+                    b.Property<int>("EnrollmentID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("CourseID");
+
+                    b.Property<int?>("Grade");
+
+                    b.Property<int>("StudentID");
+
+                    b.HasKey("EnrollmentID");
+
+                    b.HasIndex("CourseID");
+
+                    b.HasIndex("StudentID");
+
+                    b.ToTable("Enrollment");
+                });
+
+            modelBuilder.Entity("ContosoUniversitySampleApp.Models.Student", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("EnrollmentDate");
+
+                    b.Property<string>("FirstMidName");
+
+                    b.Property<string>("LastName");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Student");
+                });
+
+            modelBuilder.Entity("ContosoUniversitySampleApp.Models.Enrollment", b =>
+                {
+                    b.HasOne("ContosoUniversitySampleApp.Models.Course", "Course")
+                        .WithMany("Enrollments")
+                        .HasForeignKey("CourseID")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("ContosoUniversitySampleApp.Models.Student", "Student")
+                        .WithMany("Enrollments")
+                        .HasForeignKey("StudentID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
