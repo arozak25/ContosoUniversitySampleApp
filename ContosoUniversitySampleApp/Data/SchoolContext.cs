9f7af6c9171659caeb0c482bd5d7af832153770e
arozak25 <arozak25@gmail.com>
2017-07-19 16:23:31 +0700

    Update the database context

--- ContosoUniversitySampleApp/Data/SchoolContext.cs
+++ ContosoUniversitySampleApp/Data/SchoolContext.cs
@@ -16,6 +16,10 @@ namespace ContosoUniversitySampleApp.Data
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
 
 
         // This method gonna make the plural field name into singular in the table name on database 
@@ -24,6 +28,13 @@ namespace ContosoUniversitySampleApp.Data
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
             modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
         }
     }
 }
