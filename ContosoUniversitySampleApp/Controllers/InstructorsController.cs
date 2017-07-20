81fb29d49f5385268f8bb90f9017f8b277acae3c
arozak25 <arozak25@gmail.com>
2017-07-20 15:46:36 +0700

    Update the Delete page

--- ContosoUniversitySampleApp/Controllers/InstructorsController.cs
+++ ContosoUniversitySampleApp/Controllers/InstructorsController.cs
@@ -232,8 +232,17 @@ namespace ContosoUniversitySampleApp.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var instructor = await _context.Instructors.SingleOrDefaultAsync(m => m.ID == id);
+            Instructor instructor = await _context.Instructors
+                .Include(i => i.CourseAssignments)
+                .SingleAsync(i => i.ID == id);
+
+            var departments = await _context.Departments
+                .Where(d => d.InstructorID == id)
+                .ToListAsync();
+            departments.ForEach(d => d.InstructorID = null);
+
             _context.Instructors.Remove(instructor);
+
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
